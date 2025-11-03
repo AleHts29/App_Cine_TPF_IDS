f795873853e19e88c1f01b9f9dc89d8f85ec13c1
LukitasF1809 <lucasfratimatias@gmail.com>
2025-11-03 16:57:37 -0300

    cambios de prueba

--- frontend/app.py
+++ frontend/app.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
 @app.route('/cartelera')
 def cartelera():
-    return render_template('cartelera.html')
+    return render_template('cartelera.html', active_page='cartelera')
+
+@app.route('/login')
+def login():
+    return render_template('auth/login.html', active_page='logueo')
 
 
 if __name__ == '__main__':
+    app.config['DEBUG'] = True
     app.run(debug=True)

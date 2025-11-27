4feb4b9c8b6155fca664a7ce16585bd6058dd41b
LukitasF1809 <lucasfratimatias@gmail.com>
2025-11-27 05:24:59 -0300

    Arreglo total de cualquier error, pagina potencialmente lista para presentacion:
    CAMBIOS HECHOS:
    - rehice por completo index, ya que rompia muchas cosas
    - cambie muchisimo main.js ya que muchas funciones estaban mal cerradas
    
    COSAS FALTANTES:
    - Tema de entradas
    - Emprolijar mas el index
    - Optimizacion de logueo (distribuir bien todo)

--- frontend/static/js/main.js
+++ frontend/static/js/main.js
@@ -1,8 +1,4 @@
-const username = JSON.parse(document.body.dataset.username || "null");
-const activo = Number(document.body.dataset.activo || 0);
-
 /* -----------------SCRIPTS DE "REGISTER"------------------ */
-
 
 (async function () {
 
@@ -11,7 +7,7 @@ const activo = Number(document.body.dataset.activo || 0);
   const c2 = document.getElementById("c-password");
   const msg = document.getElementById("msg");
 
-  form.addEventListener("submit", async function (e) {
+    form.addEventListener("submit", async function(e){ 
     e.preventDefault();
     if (c1.value !== c2.value) {
       msg.textContent = "Las contraseñas no coinciden";
@@ -19,110 +15,120 @@ const activo = Number(document.body.dataset.activo || 0);
       return;
     }
 
-    const data = {
-      email: document.getElementById("email").value,
-      username: document.getElementById("name").value,
-      full_name: document.getElementById("r-name").value,
-      password: c1.value
-    };
-
-    try {
-      const response = await fetch("/usuarios", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
-      });
+      const data = {
+    email: document.getElementById("email").value,
+    username: document.getElementById("name").value,
+    full_name: document.getElementById("r-name").value,
+    password: c1.value
+      };
+    
+      try {
+        const response = await fetch("/usuarios", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(data)
+        });
 
         const result = await response.json();
 
-      if (response.ok) {
-        msg.textContent = "Se envió un correo de verificación. Revisá tu mail.";
-        msg.style.color = "green";
-        form.reset();
-        startPolling(result.id);
-      }
-      else {
-        msg.textContent = result.error || "Error al registrar usuario";
+        if (response.ok) {
+          msg.textContent = "Se envió un correo de verificación. Revisá tu mail.";
+          msg.style.color = "green";
+          form.reset();
+          startPolling(result.id);
+        } 
+        else {
+          msg.textContent = result.error || "Error al registrar usuario";
+          msg.style.color = "red";
+        }
+      } 
+      catch (err) {
+        console.error(err);
+        msg.textContent = "Error del servidor";
         msg.style.color = "red";
       }
-    }
-    catch (err) {
-      console.error(err);
-      msg.textContent = "Error del servidor";
-      msg.style.color = "red";
-    }
 
-    function startPolling(userId) {
-      const interval = setInterval(async () => {
-        try {
-          const res = await fetch(`/usuarios/status/${userId}`);
-          const data = await res.json();
-          if (data.is_active) {
-            clearInterval(interval);
-            window.location.href = "/login";
-          }
-        } catch (err) {
-          console.error("Error en el polling:", err);
+        function startPolling(userId) {
+          const interval = setInterval(async () => {
+            try {
+              const res = await fetch(`/usuarios/status/${userId}`);
+              const data = await res.json();
+              if (data.is_active) {
+                clearInterval(interval);
+                window.location.href = "/login"; 
+              }
+            } catch (err) {
+              console.error("Error en el polling:", err);
+            }
+          }, 3000); 
+
         }
-      }, 3000);
-
-    }
-
-  })
+        
+      })
 
 })();
 
 
-function sidebar_navbar() {
-  const sidebar = document.getElementById("sidebar");
-  const overlay = document.getElementById("sidebarOverlay");
+/* -----------------SCRIPTS DE "REGISTER"------------------ */
 
-  if (sidebar.classList.contains("translate-x-full")) {
-    sidebar.classList.remove("translate-x-full");
-    overlay.classList.remove("hidden");
-  } else {
-    sidebar.classList.add("translate-x-full");
-    overlay.classList.add("hidden");
-  }
+
+/* -----------------SIDEBAR DE NAVBAR------------------ */
+
+
+function sidebar_navbar() {
+    const sidebar = document.getElementById("sidebar");
+    const overlay = document.getElementById("sidebarOverlay");
+
+    if (sidebar.classList.contains("translate-x-full")) {
+        sidebar.classList.remove("translate-x-full");
+        overlay.classList.remove("hidden");
+    } else {
+        sidebar.classList.add("translate-x-full");
+        overlay.classList.add("hidden");
+    }
 }
 
 
+/* -----------------SIDEBAR DE NAVBAR------------------ */
+
+/* -----------------NAVBAR FANTASMA------------------ */
 
 
 function navbarFantasma() {
 
-  const floatNav = document.getElementById("navbar-fantasma");
-  const originalNav = document.getElementById("navbar-original");
+    const floatNav = document.getElementById("navbar-fantasma");
+    const originalNav = document.getElementById("navbar-original");
 
-  if (!floatNav || !originalNav) return;
+    if (!floatNav || !originalNav) return;
 
-  let lastScroll = 0;
+    let lastScroll = 0;
 
-  function handleScroll() {
-    const fondo = originalNav.getBoundingClientRect().bottom;
-    const scrollY = window.scrollY;
+    function handleScroll() {
+        const fondo = originalNav.getBoundingClientRect().bottom;
+        const scrollY = window.scrollY;
 
-    if (fondo < 0 && scrollY > lastScroll) {
-      floatNav.classList.remove("hidden");
-      floatNav.classList.remove("-translate-y-full");
-      floatNav.classList.add("translate-y-0");
+        if (fondo < 0 && scrollY > lastScroll) {
+            floatNav.classList.remove("hidden");
+            floatNav.classList.remove("-translate-y-full");
+            floatNav.classList.add("translate-y-0");
+        }
+
+        if (scrollY < lastScroll && fondo >= 0) {
+            floatNav.classList.add("-translate-y-full");
+            setTimeout(() => floatNav.classList.add("hidden"), 300);
+        }
+
+        lastScroll = scrollY;
     }
 
-    if (scrollY < lastScroll && fondo >= 0) {
-      floatNav.classList.add("-translate-y-full");
-      setTimeout(() => floatNav.classList.add("hidden"), 300);
-    }
-
-    lastScroll = scrollY;
-  }
-
-  window.addEventListener("scroll", handleScroll);
+    window.addEventListener("scroll", handleScroll);
 }
 document.addEventListener("DOMContentLoaded", navbarFantasma);
 
 
+/* -----------------NAVBAR FANTASMA ------------------ */
 
 
 function initSlider() {
@@ -331,17 +337,18 @@ function initButacas() {
 
 }
 
+/* -----BOTON MOSTRAR MAS EN CARTELERA ----*/
 
 function inicializarMostrarMas() {
-  const btn = document.getElementById("btnMostrarMas");
-  if (!btn) return;
+    const btn = document.getElementById("btnMostrarMas");
+    if (!btn) return; // por si estás en otra página
 
-  const extras = document.querySelectorAll(".extra-pelicula");
+    const extras = document.querySelectorAll(".extra-pelicula");
 
-  btn.addEventListener("click", () => {
-    extras.forEach(card => card.classList.remove("hidden"));
-    btn.classList.add("hidden");
-  });
+    btn.addEventListener("click", () => {
+        extras.forEach(card => card.classList.remove("hidden"));
+        btn.classList.add("hidden");
+    });
 }
 
 document.addEventListener("DOMContentLoaded", inicializarMostrarMas);

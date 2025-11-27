3bac7f3e85e0895c9000facbf6662f4b613c9f1c
Alejandro Huertas <alejo29h@gmail.com>
2025-11-26 23:41:17 -0300

    sync

--- backend/db.py
+++ backend/db.py
@@ -6,15 +6,19 @@ from mysql.connector import Error
 load_dotenv()
 
 def get_connection():
+    try:
+        conn = mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME")
+        )
 
-        try:
-            conn = mysql.connector.connect(
-                host=os.getenv("DB_HOST", "localhost"),
-                user=os.getenv("DB_USER", "root"),
-                password=os.getenv("DB_PASSWORD", ""),
-                database=os.getenv("DB_NAME", "")
-            )
-            return conn
-        
-        except Error as e:
-            print(f"Error conectando a la base de datos: {e}")
+        if conn.is_connected():
+            print("Conexión exitosa a MySQL")
+        return conn
+
+    except Error as e:
+        print(f"❌ Error conectando a la base de datos: {e}")
+        return None
